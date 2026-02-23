7b993c9aef15ca670e89c8c1d1a35ba204594819
Graham Crosbie <grahamjcros@gmail.com>
2026-02-23 10:34:24 -0700

    update script for exit

--- Assets/Scripts/LevelTrigger.cs
+++ Assets/Scripts/LevelTrigger.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
 
 public class LevelTrigger : MonoBehaviour
 {
     public string nextSceneName;
+    public GameObject scoreDisplayObject;
+    public TextMeshProUGUI scoreText;
+    public float displayDuration = 5f;
+
+    private ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = GameObject.FindWithTag("Score Manager").GetComponent<ScoreManager>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(nextSceneName);
+            StartCoroutine(ShowScoreAndTransition());
         }
+    }
+
+    private IEnumerator ShowScoreAndTransition()
+    {
+        scoreDisplayObject.SetActive(true);
+        scoreText.text = "Level Complete! Score: " + scoreManager.score;
+
+        yield return new WaitForSeconds(displayDuration);
+
+        SceneManager.LoadScene(nextSceneName);
     }
 }
