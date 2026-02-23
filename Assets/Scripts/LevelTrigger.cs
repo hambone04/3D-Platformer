17c29c4e03b8c4ceb8004db54b65842bf4594e76
Graham Crosbie <grahamjcros@gmail.com>
2026-02-23 10:21:45 -0700

    level 2 beginnings

---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTrigger : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
